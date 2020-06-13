1b5cc411a369f3f1bc40d4b256a5103ef8054664
orby-tech <tibode495@gmail.com>
2020-06-13 18:14:56 +0300

    some step

--- simpleElements/sum.test.js
+++ simpleElements/sum.test.js
@@ -6,3 +6,16 @@ test ( "1 + 2 -> 3 " , () => {
     expect(sum(1, 4)).not.toBe(2)
     expect(sum(1, 4)).not.toBeGreaterThan(6)
 })
+
+test('the data is peanut butter', done => {
+    function callback(data) {
+      try {
+        expect(data).toBe('peanut butter');
+        done();
+      } catch (error) {
+        done(error);
+      }
+    }
+  
+    fetchData(callback);
+  });
